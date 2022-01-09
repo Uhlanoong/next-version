0ce63040aa86c775af0227a34406fc84815276ac
saif1642 <islumsaiful@gmail.com>
2022-01-09 17:40:15 +0600

    soulution page updated

--- src/Pages/Solution/Solution.js
+++ src/Pages/Solution/Solution.js
@@ -1,50 +1,39 @@
-/* eslint-disable no-unused-vars */
-
-import React, { useEffect, useState,} from 'react';
+/* eslint-disable no-undef */
+import React, { useEffect, useState, useCallback } from 'react';
 import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap';
 import './Solution.css';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import '../Stackoverflow/Stackoverflow';
 import Results from '../Age/Results';
-// import Tablerow from '../Row/Tablerow';
-import SolutionRow from './SolutionRow';
-
-
+import TrComponent from '../../components/TrComponent';
+// import Result from '../Age/Result';
 
 const Solution = () => {
     // Output formula
     const [waterArea, setWaterArea] = useState(0);
     const [waterDepth, setWaterDepth] = useState(0);
-    const [volume, setVolume] = useState(0);
+    const [volume, setVolume] = useState('');
     const [production, setProduction] = useState('');
     const [oxygendemand, setOxygenDemand] = useState(0);
-    const [oxygenHarvestdemand, setOxygenHarvestDemand] = useState(0);
-    const [oxygentwodemand, setOxygenTwoDemand] = useState(0);
 
     //Table formula
     const [totalPc, setTotalPc] = useState(null);
     const [stockingDensity, setStockingDensity] = useState(null);
     const [presentSize, setPresentSize] = useState(null)
-    const [harvestSize, setHarvestSize] = useState(null);
     
-    const [totalWeight, setTotalWeight] = useState('');
-    const [totalHarvestWeight, setTotalHarvestWeight] = useState('');
+    const [totalWeight, setTotalWeight] = useState('')
     const [presentBiomass, setPresentBiomass] = useState('');
+
+    const [rows, setRows] = useState(1);
+    
     
     const handlePresentSize = (e)=>{
         setPresentSize(e.target.value)
     }
-    const handleHarvestSize = (e)=>{
-        setHarvestSize(e.target.value)
-    }
     
     const handleTotalWeight = (e)=>{
-        setTotalWeight(e.target.value)
-    }
-    
-    const handleTotalHarvestWeight = (e)=>{
-        setTotalHarvestWeight(e.target.value)
+        // setTotalWeight(e.target.value)
     }
     const handleTotalPc = (e)=>{
         setTotalPc(e.target.value);
@@ -57,16 +46,16 @@ const Solution = () => {
     }
 
     useEffect(()=>{
-        let cubic = parseFloat("28.32").toFixed(2)
+        let cubic = parseFloat(28.32).toFixed(2)
         let newVolume = waterArea * waterDepth * cubic
-        setVolume(newVolume.toFixed(2))
+        setVolume(newVolume)
     }, [waterArea, waterDepth])
     
     useEffect(() => {
         let range = parseFloat("1.45830299").toFixed(2);
         let cubic = parseFloat("28.32").toFixed(2)
         let newVolume = waterArea * waterDepth * cubic
-        setVolume(newVolume.toFixed(2))
+        setVolume(newVolume)
         let oxygenProduction = newVolume* range
         setProduction(oxygenProduction.toFixed(2))
     }, [waterArea, waterDepth, production]);
@@ -83,12 +72,6 @@ const Solution = () => {
     }, [totalPc, presentSize]);
 
     useEffect(() => {
-        let newTotalHarvestWeight = (totalPc * harvestSize) /1000
-        setTotalHarvestWeight(newTotalHarvestWeight)
-        
-    }, [totalPc, harvestSize]);
-
-    useEffect(() => {
         let newPresentBiomass = totalWeight /waterArea
         setPresentBiomass(newPresentBiomass)
     }, [totalWeight, waterArea]);
@@ -99,73 +82,315 @@ const Solution = () => {
       stockingdate: '',
       showResults: false,
     });
-    const CalculateAge = () => {
-      setData({ stockingdate: data.date, showResults: true });
+    const CalculateAge = (inputDate) => {
+      var diffInTime = new Date(inputDate).getTime() - new Date().getTime();
+      var days = Math.ceil(diffInTime / (1000 * 3600 * 24));
+      return Math.abs(days);
     };
     const changeHandler = (e) => {
       setData({ date: e.target.value });
     };
 
+    // eslint-disable-next-line no-unused-vars
+    const [choices, setChoices] = useState([
+      {
+        name: "Select…",
+        value: null,
+      },
+      {
+        name: "Rui",
+        value: 3000,
+        age: {
+          day1: 0,
+          day15: 5,
+          day30: 15,
+          day45: 25,
+          day60: 55,
+          day75: 95,
+          day90: 150,
+          day105: 210,
+          day120: 175,
+          day135: 350,
+          day150: 430,
+          day165: 515,
+          day180: 610,
+        },
+      },
+      {
+        name: "Catla",
+        value: 3000,
+        age: {
+          day1: 0,
+          day15: 5,
+          day30: 15,
+          day45: 25,
+          day60: 55,
+          day75: 95,
+          day90: 150,
+          day105: 210,
+          day120: 175,
+          day135: 350,
+          day150: 430,
+          day165: 515,
+          day180: 610,
+        }
+      },
+      {
+        name: "Carpio",
+        value: 3000,
+        age: {
+          day1: 0,
+          day15: 5,
+          day30: 15,
+          day45: 25,
+          day60: 55,
+          day75: 95,
+          day90: 150,
+          day105: 210,
+          day120: 175,
+          day135: 350,
+          day150: 430,
+          day165: 515,
+          day180: 610,
+        }
+      },
+      {
+        name: "Tilapia",
+        value: 1750,
+      },
+      {
+        name: "Mrigel",
+        value: 3000,
+        age: {
+          day1: 0,
+          day15: 5,
+          day30: 15,
+          day45: 25,
+          day60: 55,
+          day75: 95,
+          day90: 150,
+          day105: 210,
+          day120: 175,
+          day135: 350,
+          day150: 430,
+          day165: 515,
+          day180: 610,
+        }
+      },
+      {
+        name: "Pabda",
+        value: 3500,
+      },
+      {
+        id: '8',
+        name: "Pangasius",
+        age: {
+          day1: 30,
+          day15: 79,
+          day30: 133,
+          day45: 196,
+          day60: 265,
+          day75: 343,
+          day90: 430,
+          day105: 523,
+          day120: 616,
+          day135: 712,
+          day150: 808,
+          day165: 904,
+          day180: 1000,
+        }
+        
+      },
+      {
+        name: "Koi",
+        value: 750,
+        age: {
+          day1: 0.25,
+          day15: 5,
+          day30: 10,
+          day45: 20,
+          day60: 35,
+          day75: 55,
+          day90: 80,
+          day105: 110,
+          day120: 150,
+        }
+      },
+    ]);
+    // const [age, setAge] = useState("");
+  
+    // const handleAgeChange = useCallback((e) => {
+    //   setAge(e.target.value);
+    // }, []);
+  
+
+    // eslint-disable-next-line no-unused-vars
     const [options, setOptions] = useState([
-      { id: '1', name: "Select…", value: 0, upperlayer: "",},
-      { id: '2', name: "Rui", value: 3000, upperlayer: "",},
-      { id: '3', name: "Catla", value: 3000, upperlayer: "Catla",},
-      { id: '4', name: "Carpio", value: 3000,},
-      { id: '5', name: "Silver Carp", value: 3000,},
-      { id: '6', name: "Grass Carp", value: 3000,},
-      { id: '7', name: "Black Carp", value: 3000,},
-      { id: '8', name: "Big Head", value: 3000,},
-      { id: '9', name: "Tilapia", value: 1750,},
-      { id: '10', name: "Mrigel", value: 3000,},
-      { id: '11', name: "Pabda", value: 3500,},
-      { id: '12',name: "Pangasius", value: 350,},
-      { id: '13', name: "Koi",value: 750,},
-      { id: '14', name: "Shing",value: 7500,},
-      { id: '15', name: "Magur",value: 7500,},
-      { id: '16', name: "Gulsha",value: 7500,},
-      { id: '17', name: "Bagdha",value: 7500,},
-      { id: '18', name: "Goldha",value: 7500,},
-    ]);
-    const [fishes, setFishes] = useState([
-      { id: '11', name: "Select…", value: 0,},
-      { id: '12', name: "Rui", value: 3000,},
-      { id: '13', name: "Catla", value: 3000,},
-      { id: '14', name: "Carpio", value: 3000,},
-      { id: '15', name: "Tilapia", value: 1750,},
-      { id: '16', name: "Mrigel", value: 3000,},
-      { id: '17', name: "Pabda", value: 3500,},
-      {id: '18',name: "Pangasius",value: 0},
-      {id: '19', name: "Koi",value: 750,},
-    ]);
+        { id: '1',
+          name: "Select…",
+          value: 0,
+          age:{}
+        },
+        { 
+          id: '2',
+          name: "Rui",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        {
+          id: '3',
+          name: "Catla",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        { 
+          id: '4',
+          name: "Carpio",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        { 
+          id: '5',
+          name: "Tilapia",
+          value: 1750,
+          age: {
+            day1: 20,
+            day15: 35,
+            day30: 55,
+            day45: 80,
+            day60: 110,
+            day75: 150,
+            day90: 200,
+            day105: 260,
+            day120: 330,
+            day135: 405,
+            day150: 490,
+            day165: 585,
+            day180: 700,
+          }
+        },
+        {
+          id: '6',
+          name: "Mrigel",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        {
+          id: '7',
+          name: "Pabda",
+          value: 3500,
+          
+        },
+        {
+          id: '8',
+          name: "Pangasius",
+          age: {
+            day1: 30,
+            day15: 79,
+            day30: 133,
+            day45: 196,
+            day60: 265,
+            day75: 343,
+            day90: 430,
+            day105: 523,
+            day120: 616,
+            day135: 712,
+            day150: 808,
+            day165: 904,
+            day180: 1000,
+          }
+          
+        },
+        {
+          id: '9',
+          name: "Koi",
+          value: 750,
+          age: {
+            day1: 0.25,
+            day15: 5,
+            day30: 10,
+            day45: 20,
+            day60: 35,
+            day75: 55,
+            day90: 80,
+            day105: 110,
+            day120: 150,
+          }
+        },
+      ]);
 
-    const [value, setValue] = useState("");
-    const [twovalue, setTwoValue] = useState("");
+      
+      const [value, setValue] = useState("");
+      
 
-    const handleChange = (e) => {
-      setValue(e.target.value);
-      console.log(e.target.value);
-    };
-    const handleTwoChange = (e) => {
-      setTwoValue(e.target.value);
-      console.log(e.target.value);
-    };
-
+      const handleChange = useCallback((e) => {
+        setValue(e.target.value);
+      }, []);
+      
+    
+      
       useEffect(() => {
         let newOxygenDemand = value * totalWeight
         setOxygenDemand(newOxygenDemand)
      }, [value, totalWeight]);
 
-      useEffect(() => {
-        let newOxygenHarvestDemand = value * totalHarvestWeight
-        setOxygenHarvestDemand(newOxygenHarvestDemand)
-     }, [value, totalHarvestWeight]);
-
-      useEffect(() => {
-        let newOxygenTwoDemand = twovalue * totalWeight
-        setOxygenTwoDemand(newOxygenTwoDemand)
-     }, [twovalue, totalWeight]);
-
-    
+     
     return (
         <div className = "my-2">
             <Container>
@@ -184,19 +409,19 @@ const Solution = () => {
                         </Form.Group>
                         
                         <Form.Group as={Col} controlId="formGridProblembatic Species">
-                        {/* <select onChange={handleZoneChange} value={upperlayer}>
+                        {/* <select onChange={handleAgeChange} value={age}>
                             {choices.map((item) => (
-                            <option key={item.id} value={item.upperlayer}>
+                            <option key={item.id} value={item.age}>
                                 {item.name}
                             </option>
                             ))}
                         </select> */}
                         <Autocomplete
-                        disablePortal
-                        id=""
-                        options={fish}
-                        sx={{ width: 260}}
-                        renderInput={(params) => <TextField {...params} label="Species" size = "small"/>}
+                            disablePortal
+                            id=""
+                            options={fish}
+                            sx={{ width: 260}}
+                            renderInput={(params) => <TextField {...params} label="Species" size = "small"/>}
                         />
                         </Form.Group>
                     </Row>
@@ -209,14 +434,14 @@ const Solution = () => {
                         <>
                         {data.showResults ? <Results date={data.stockingdate} /> : <div />}
                         </>
-                        <Form.Group as={Col} controlId="formCalculation">
-                        <Button
-                          variant="primary" size="md" active
-                          onClick={CalculateAge}>
-                          Calculate
-                        </Button>
-                        
-                        </Form.Group>
+                        {
+                          data.date !== '' &&  
+                          <Form.Group as={Col} controlId="formCalculation">
+                            <Form.Control type="text" readOnly placeholder="Age" value={CalculateAge(data.date)}/>
+                          </Form.Group>
+                        }
+
+
                         <Form.Group as={Col} controlId="formGridStockingQuantity">
                         <Form.Control type = "number" placeholder = "Stocking Quantity" />
                         </Form.Group>
@@ -235,14 +460,15 @@ const Solution = () => {
                         <th>Stk Dty</th>
                         <th>Ttl Wt(kg)</th>
                         <th>Hvst Size</th>
-                        <th>Hvst Wt</th>
                         <th>Psnt Biomass</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                        [" ", " ", " ",].map(item=>(<SolutionRow />))
-                        }
+                          
+                        {Array(rows).fill(null).map((value, index) => (
+                            <TrComponent  key={index} rowId={index} ChangedTotalWeight={(totalWeight) => console.log("totalWeight")}/>
+                        ))}
+                          
                     </tbody>
                     </Table>
                     </Col>
@@ -252,11 +478,11 @@ const Solution = () => {
                     <br/>
                     O<sub>2</sub> Production: {production} mg/hr
                     <br/>
-                    Total O<sub>2</sub> demand: {(oxygendemand + oxygentwodemand)} mg
+                    Total O<sub>2</sub> demand: {oxygendemand} mg
                     {/* <br/>
                     Bodyweight: {age} */}
                     <br/>
-                    Total O<sub>2</sub> demand harvest size: {oxygenHarvestdemand} mg
+                    Total O<sub>2</sub> demand harvest size:
                     <br/>
                     Zonewise Fish Distribution:
                     <br/>
@@ -268,12 +494,13 @@ const Solution = () => {
                     </p>
                     </Col>
                     <Button type = "submit" variant = "success" className = "mx-2" size = "sm" style = {{width: "100px"}}>Save</Button>
+                    <Button type = "button" onClick={() => {setRows(rows+1)}} variant = "info" className = "mx-2" size = "sm" style = {{width: "100px"}}>Add New</Button>
+
                 </Row>
             </Container>
         </div>
     );
 };
-
 const fish = [
   
     { label: 'Tilapia',
---
+++ src/components/TrComponent.js
@@ -0,0 +1,222 @@
+import React, { useEffect, useState, useCallback } from 'react';
+
+const TrComponent = (props) => {
+    const [optionValue, setOptionValue] = useState(0);
+    const [totalWeight, setTotalWeight] = useState('')
+    const [presentBiomass, setPresentBiomass] = useState('');
+    const [stockingDensity, setStockingDensity] = useState(null);
+    const [presentSize, setPresentSize] = useState(0);
+    const [totalPc, setTotalPc] = useState(0);
+
+
+    const [options, setOptions] = useState([
+        { id: '1',
+          name: "Select…",
+          value: 0,
+          age:{}
+        },
+        { 
+          id: '2',
+          name: "Rui",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        {
+          id: '3',
+          name: "Catla",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        { 
+          id: '4',
+          name: "Carpio",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        { 
+          id: '5',
+          name: "Tilapia",
+          value: 1750,
+          age: {
+            day1: 20,
+            day15: 35,
+            day30: 55,
+            day45: 80,
+            day60: 110,
+            day75: 150,
+            day90: 200,
+            day105: 260,
+            day120: 330,
+            day135: 405,
+            day150: 490,
+            day165: 585,
+            day180: 700,
+          }
+        },
+        {
+          id: '6',
+          name: "Mrigel",
+          value: 3000,
+          age: {
+            day1: 0,
+            day15: 5,
+            day30: 15,
+            day45: 25,
+            day60: 55,
+            day75: 95,
+            day90: 150,
+            day105: 210,
+            day120: 175,
+            day135: 350,
+            day150: 430,
+            day165: 515,
+            day180: 610,
+          }
+        },
+        {
+          id: '7',
+          name: "Pabda",
+          value: 3500,
+          
+        },
+        {
+          id: '8',
+          name: "Pangasius",
+          age: {
+            day1: 30,
+            day15: 79,
+            day30: 133,
+            day45: 196,
+            day60: 265,
+            day75: 343,
+            day90: 430,
+            day105: 523,
+            day120: 616,
+            day135: 712,
+            day150: 808,
+            day165: 904,
+            day180: 1000,
+          }
+          
+        },
+        {
+          id: '9',
+          name: "Koi",
+          value: 750,
+          age: {
+            day1: 0.25,
+            day15: 5,
+            day30: 10,
+            day45: 20,
+            day60: 35,
+            day75: 55,
+            day90: 80,
+            day105: 110,
+            day120: 150,
+          }
+        },
+    ]);
+
+    const handleChange = useCallback((e) => {
+        setOptionValue(e.target.value);
+    }, []);
+    
+    
+    const handleTotalWeight = (e)=>{
+        setTotalWeight(e.target.value);
+        props.ChangedTotalWeight(e.target.value);
+        // console.log(e.target.value);
+    }
+    const handleStockingDensity = (e)=>{
+        setStockingDensity(e.target.value);
+    }
+    const handlePresentBiomass = (e)=>{
+        setPresentBiomass(e.target.value)
+    }
+
+    const handlePresentSize = (e)=>{
+        setPresentSize(e.target.value);
+    }
+
+    const handleTotalPc = (e)=>{
+        setTotalPc(e.target.value);
+    }
+
+    // function updateTotalWeight(){
+    //     console.log(totalPc,presentSize);
+    //     setTotalWeight(totalPc * presentSize);
+    // }
+
+    useEffect(() => {
+        let newTotalWeight = (totalPc * presentSize) /1000
+        setTotalWeight(newTotalWeight)
+        
+    }, [totalPc, presentSize]);
+    
+
+    return (
+            <tr>
+                <td>{props.rowId}</td>
+                <td>
+                <select onChange={handleChange} value={optionValue}>
+                    {options.map((item) => (
+                        <option key={item.id} value={item.value}>
+                            {item.name}
+                        </option>
+                    ))}
+                </select>
+                </td>
+                <td><input type = "number" maxLength = "5" size = "5" min="1" max="5" onChange={handleTotalPc} value={totalPc}></input></td>
+                <td><input type = "number" style={{width: "80px"}} ></input></td>
+                <td><input type = "number" style={{width: "80px"}} onChange={handlePresentSize} value={presentSize}></input></td>
+                <td><input type = "number" min="1" max="5" onChange= {handleStockingDensity} value={stockingDensity}></input></td>
+                <td><input type = "number" style={{width: "70px"}} onChange={handleTotalWeight} value={totalWeight}></input></td>
+                <td><input type = "number" style={{width: "80px"}} ></input></td>
+                <td><input type = "number" style={{width: "80px"}} onChange={handlePresentBiomass} value={presentBiomass}></input></td>
+            </tr>
+    );
+};
+
+export default TrComponent;
